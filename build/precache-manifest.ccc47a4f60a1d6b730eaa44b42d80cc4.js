701858399073e4df0637fa8c60c9dc03981c1cac
Shabia <shabz19@gmail.com>
2020-12-22 15:11:51 +0000

    added build folder

---
+++ build/precache-manifest.ccc47a4f60a1d6b730eaa44b42d80cc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dfb63ef3889e07889098a0171ba78e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5e7064e79b40e1547fd",
+    "url": "/static/css/main.50d97835.chunk.css"
+  },
+  {
+    "revision": "89d7c231c6aa35668c45",
+    "url": "/static/js/2.62600763.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.62600763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e7064e79b40e1547fd",
+    "url": "/static/js/main.28ac9154.chunk.js"
+  },
+  {
+    "revision": "5acee2345230f972cbdb",
+    "url": "/static/js/runtime-main.73bddb69.js"
+  },
+  {
+    "revision": "a4c2fef5770bf0d6dee3c317e0ea386b",
+    "url": "/static/media/hpBg.a4c2fef5.png"
+  }
+]);
