2dce7793f4f34e0d01795040dcd3438d072a8fdc
Shabia <shabz19@gmail.com>
2021-01-14 21:27:30 +0000

    build please

---
+++ build/precache-manifest.a7b9a8fbb87feaf662a6a3c72710f2e5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f007a1577dd40eaa5d0d413cd0233db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdfe65dc87780c42ac4b",
+    "url": "/static/css/main.81d1d6ee.chunk.css"
+  },
+  {
+    "revision": "415efc9ee0bb50089ce5",
+    "url": "/static/js/2.37149e99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.37149e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdfe65dc87780c42ac4b",
+    "url": "/static/js/main.83e10a48.chunk.js"
+  },
+  {
+    "revision": "5acee2345230f972cbdb",
+    "url": "/static/js/runtime-main.73bddb69.js"
+  },
+  {
+    "revision": "a03d02a6ce932f5e9f535e6cdd40d046",
+    "url": "/static/media/2ppl.a03d02a6.png"
+  },
+  {
+    "revision": "1889400d417f84ea793c0492e5314f62",
+    "url": "/static/media/3ppl.1889400d.png"
+  },
+  {
+    "revision": "8653da0a0f099fb8d780298e5fb90d4d",
+    "url": "/static/media/FR-Design1Flow.8653da0a.svg"
+  },
+  {
+    "revision": "91ed368d9a98b2f3be94fc259d480943",
+    "url": "/static/media/FR-Design2Flow.91ed368d.svg"
+  },
+  {
+    "revision": "ae9e55057ffa061592bd9bfaca7acc93",
+    "url": "/static/media/FR-IdeaBoard.ae9e5505.svg"
+  },
+  {
+    "revision": "5665f99a8e40922c2908798bb7948ef9",
+    "url": "/static/media/FR-Process.5665f99a.svg"
+  },
+  {
+    "revision": "0cb8403e124d78106b49f66dc16b3ebd",
+    "url": "/static/media/profilepic.0cb8403e.png"
+  },
+  {
+    "revision": "2af36872025822e43d8e3595c2acf2f2",
+    "url": "/static/media/rect.2af36872.png"
+  },
+  {
+    "revision": "a77ddc7a025eb0c6afe7845685d50434",
+    "url": "/static/media/test-img.a77ddc7a.png"
+  }
+]);
