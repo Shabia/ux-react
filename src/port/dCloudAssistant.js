7cb3dbfcd351da25ab023aa31214cc5a4ff45dd5
Shabia <shabz19@gmail.com>
2021-02-09 21:46:12 +0000

    started dcloud assistant

--- src/port/dCloudAssistant.js
+++ src/port/dCloudAssistant.js
@@ -2,22 +2,17 @@ import React from 'react'
 import {Row, Nav, Col, Card, CardGroup, Container} from 'react-bootstrap';
 import { HashLink as Link } from 'react-router-hash-link';
 import threeppl from './../assets/3ppl.png'
-import frprocess from './../assets/FR-Process.svg'
-import frIdea from './../assets/FR-IdeaBoard.svg'
-import frFlowA from './../assets/FR-Design1Flow.svg'
-import frFlowB from './../assets/FR-Design2Flow.svg'
-import frwirea from './../assets/FR-Wire1.svg'
-import frwireba from './../assets/FR-Wire2A.svg'
-import frwirebb from './../assets/FR-Wire2B.svg'
-import frwirebc from './../assets/FR-Wire2C.svg'
-import frdrawone from './../assets/FR-Draw1.svg'
-import frwdrawtwo from './../assets/FR-Draw2.svg'
+import cbprocess from './../assets/CB-Process.svg'
+import cbwire1 from './../assets/CB-Wire1.png'
+import cbwire2 from './../assets/CB-Wire2.png'
+import cbwire3 from './../assets/CB-Wire3.png'
 import frnewreq from './../assets/FR-NewRequest.gif'
 import frupvote from './../assets/FR-UpVote.gif'
 import frreadcomment from './../assets/FR-ReadComment.gif'
 import fraddcomment from './../assets/FR-AddComment.gif'
 import frreplycomment from './../assets/FR-ReplyComment.gif'
 import cb from './../assets/CB.svg'
+import cbflow from './../assets/CB-Flow.svg'
 
 export const dCloudAssistant = () => (
   <div>
@@ -28,7 +23,7 @@ export const dCloudAssistant = () => (
     <Col md={6} className="py-5">
             <h1 className="display-6">dCloud Assistant</h1>
             <blockquote class="blockquote title-border my-4">
-              <p>App to schedule and interact with dCloud without a UI via using bot commands / voice</p>
+              <p>App to schedule and interact with dCloud using messenger bot commands / voice</p>
             </blockquote>
         
               <Nav defaultActiveKey="/home" className="flex-column">
@@ -49,7 +44,7 @@ export const dCloudAssistant = () => (
  <section className="port-section" >
  <h2 className="text-center">Process</h2>
  <div className="text-center">
- <img src={frprocess} alt="Process" className="img-fluid"/> 
+ <img src={cbprocess} alt="Process" className="img-fluid"/> 
  </div>
  </section>
 
@@ -60,8 +55,8 @@ export const dCloudAssistant = () => (
     <Col md={8} className="sub-content">
     <p>We found that people liked to schedule on the go and some super users used dcloud Apis to create their own 
       scheduling bot to bypass the UI which they thought was long winded. When using the dCloud catalog we found 
-      that a bunch of users created a command list via Cisco Webex API’s to be able.</p>
-      <p>We thought it would be a nice experiment to build something official out for them</p>
+      that a bunch of users created a command list via Cisco Webex API’s to be able to find content and schedule it.</p>
+      <p>We thought it would be a nice experiment to build something official out for them and try out natural language</p>
 
     </Col>
     </Row>
@@ -114,8 +109,6 @@ export const dCloudAssistant = () => (
           <dl>
             <dt>Method  and Tools</dt>
             <dd>Figma Design -  Wireframe + UI Design</dd>
-            <dd>Adobe Sketch -  Rapid prototype ideas.</dd>
-            <dd>Miro - User flows, Requirements Analysis</dd>
           </dl>
           </Col>
         </Row>
@@ -135,86 +128,100 @@ export const dCloudAssistant = () => (
     <Row className="sect-row">
     <Col md={3} className="sub-section"><h3 className="title-border">Talk to Users</h3></Col>
     <Col md={8} className="sub-content">
-      <p>Designers talked about the features we wanted and how best to capture the info. 
-        We wanted to be basic and give us the info back without having the need to analyse requests.</p>
+      <p>Learn what kind of tasks they typically perform and why they do it</p>
+      <h5 className="mb-3 mt-5">Typical Tasks</h5>
         <ul>
-          <li>Organised ideas into buckets - Important/Nice to have/Overkill</li>
-          <li>Reasons for ideas - Why</li>
-          <li>Two user types - admin (us) topolgy users</li>
+          <li>Find a demo </li>
+          <li>Schedule content - date time</li>
+          <li>Find out what they have booked</li>
+          <li>Test internet connection</li>
+          <li>See most scheduled content</li>
         </ul>
-        <img src={frIdea} alt="Feature Request Ideas Board" className="img-fluid"/> 
     </Col>
     </Row>
-   {/* <Row className="sect-row">
-    <Col md={3} className="sub-section"><h3 className="title-border">Feature mapping &amp; user flow</h3></Col>
-    <Col md={8} className="sub-content"> 
+    <Row className="sect-row">
+    <Col md={3} className="sub-section"><h3 className="title-border">Command User Journey</h3></Col>
+    <Col md={8} className="sub-content">
+    <img src={cbflow} alt="COmmand flow diagram" className="img-fluid"/> 
+ 
     </Col>
-</Row>*/}
+    </Row>
+
     <h3 className="title-border">Prototyping</h3>
  
     <Row className="sect-row">
-    <Col md={3} className="sub-section"><h4>User view Design 1</h4></Col>
+    <Col md={3} className="sub-section"><h4>First Iteration</h4></Col>
     <Col md={8} className="sub-content"> 
-    <h6 className="mb-3">Flow diagram with basic required features</h6>
-    <img src={frFlowA} alt="Flow A" className="img-fluid"/> 
-    <h6 className="mb-3 mt-5">Sketched Iteration</h6>
-      <img src={frdrawone} alt="hand drawn sketch one" className="img-fluid"/> 
-      <h6 className="mb-3 mt-5">Wireframe</h6>
-      <img src={frwirea} alt="Wireframe design of first iteration" className="img-fluid"/> 
-     
-      </Col>
-      
-    </Row>
-    <Row className="sect-row">
-    <Col md={3} className="sub-section"><h5>Thoughts &amp; discussion</h5></Col>
-    <Col md={8} className="sub-content"><p>Thoughts screen is a bit busy
-        We want to reduce submissions of the same thing so need a way to show other peoples submission
-        Maybe commenting on submissions and for explaining any further views - encourages discussions and conversations
-        A way to show we are working on the submissions - keeps customers engaged and happy</p></Col>
-    </Row>
-    <Row className="sect-row">
-    <Col md={3} className="sub-section"><h4>User view Design 2</h4></Col>
-    <Col md={8} className="sub-content">
-    <h6 className="mb-3">Second pass with additional features</h6>
-    <img src={frFlowB} alt="Flow B" className="img-fluid"/> 
-    <h6 className="mb-3 mt-5">Sketched page, request form modal, comment/reply idea, request card info</h6>
-      <img src={frwdrawtwo} alt="hand drawn sketch two" className="img-fluid"/> 
-      <h5 className="mb-3 mt-5">Wireframes of second designs</h5>
-      <h6 className="mb-3">Landing Page</h6>
-      <img src={frwireba} alt="Wireframe design of second iteration - landing page" className="img-fluid"/> 
-      <h6 className="mb-3 mt-5">Submit request modal</h6>
-      <img src={frwirebc} alt="Wireframe design of second iteration"/> 
-      <h6 className="mb-3 mt-5">Commenting and Replying</h6>
-      <img src={frwirebb} alt="Wireframe design of second iteration" className="img-fluid"/> 
+    <h5 className="mb-3">Basic idea to get started</h5>
+    <p>User initiates the conversation by telling the bot what they want </p>
+    <p>The bot ultra smart in having a conversation and responds pretty well</p>
+    <img src={cbwire1} alt="Wireframe 1" className="img-fluid"/> 
+    <p>Pros / Cons</p>
+    <ul className="list-unstyled">
+      <li>+ Good to give option to type or speak</li>
+      <li>+ Use of natural language by bot</li>
+      <li>- Reponses from bot are too long</li>
+      <li>- Bot is not going to be that smart so we have to change how the responses work</li>
+    </ul>
+     </Col>
+  </Row>
 
-      
-      </Col>
-    </Row>
-    <Row className="sect-row">
-    <Col md={3} className="sub-section"><h5>Thoughts &amp; discussion</h5></Col>
-    <Col md={8} className="sub-content"><p>Thoughts screen is a bit busy
-        We want to reduce submissions of the same thing so need a way to show other peoples submission
-        Maybe commenting on submissions and for explaining any further views - encourages discussions and conversations
-        A way to show we are working on the submissions - keeps customers engaged and happy</p></Col>
-    </Row>
+  <Row className="sect-row">
+    <Col md={3} className="sub-section"><h4>Second Iteration</h4></Col>
+    <Col md={8} className="sub-content"> 
+    <h5 className="mb-3">Adding commands and reducing language complexity</h5>
+    <p>Main goal is to make interactions very simple with basic responses like yes/no so that it's easy to understand for the bot </p>
+    <p>The responses from the bot are simplified as we want to reduce time spent talking</p>
+    <p>Introduction of tasks as commands</p>
+    <img src={cbwire2} alt="Wireframe 2" className="img-fluid"/> 
+    <p>Pros / Cons</p>
+    <ul className="list-unstyled">
+      <li>+ List of tasks useful</li>
+      <li>+ Option to handle error e.g. failed connection test with exit (reschedule)</li>
+      <li>+ Yes no answers make it quick</li>
+      <li>- How do you even use tasks quickly?</li>
+    </ul>
+     </Col>
+  </Row>
+  <Row className="sect-row">
+    <Col md={3} className="sub-section"><h4>Third Iteration</h4></Col>
+    <Col md={8} className="sub-content"> 
+    <h5 className="mb-3">Simplyfying Commands and responses</h5>
+    <p>Adding number value to tasks to make it fast and easy to respond </p>
+    <p>Introduce how a support agent enters the chat and takes over from bot</p>
+    <p>Introduction of tasks as commands</p>
+    <img src={cbwire3} alt="Wireframe 3" className="img-fluid"/> 
+    <p>Pros / Cons</p>
+    <ul className="list-unstyled">
+      <li>+ Number commands reduce times significantly</li>
+      <li>+ Nice to have support agent take over</li>
+      <li>+ Recognition of? </li>
+      <li>- Need to see interaction </li>
+    </ul>
+     </Col>
+  </Row>
+
     <Row className="sect-row">
     <Col md={3} className="sub-section"><h3 className="title-border">Interactions &amp; Visuals</h3></Col>
     <Col md={8} className="sub-content">
-      Visuals designed in Figma using:
+      <h4>Putting it all together now</h4>
+      <p>Visuals designed in Figma using:</p>
       <ul>
-        <li>Cisco UI Kit - as a base</li>
-        <li>Topology Builder UI design - design language for this suite of products that we previously designed</li>
+        <li>Cisco UI design language as a base</li>
+        <li>Figma interactions</li>
         </ul>
        
-        <h6 className="mb-3">New Request</h6>
+        <h5 className="mb-3">Start Using App</h5>
         <img src={frnewreq} alt="Create a new request" className="img-fluid mb-2 ux-gif"/> 
-        <h6 className="mb-3 mt-5">Upvote</h6>
+        <h5 className="mb-3 mt-5">See Schedule</h5>
         <img src={frupvote} alt="Upvote a request" className="img-fluid mb-2 ux-gif"/> 
-        <h6 className="mb-3 mt-5">Read Comments</h6>
+        <h5 className="mb-3 mt-5">Book a Session</h5>
         <img src={frreadcomment} alt="Read Comments" className="img-fluid mb-2 ux-gif"/> 
-        <h6 className="mb-3 mt-5">Add Comment</h6>
+        <h5 className="mb-3 mt-5">Test Connection</h5>
         <img src={fraddcomment} alt="Add Comment" className="img-fluid mb-2 ux-gif"/> 
-        <h6 className="mb-3 mt-5">Reply to Comment</h6>
+        <h5 className="mb-3 mt-5">Search</h5>
+        <img src={frreplycomment} alt="Reply to Comment" className="img-fluid mb-2 ux-gif"/> 
+        <h5 className="mb-3 mt-5">Contact Support</h5>
         <img src={frreplycomment} alt="Reply to Comment" className="img-fluid mb-2 ux-gif"/> 
    
    
@@ -228,7 +235,7 @@ export const dCloudAssistant = () => (
  <section className="port-section" id="outcomes" >
  <h2 className="text-center">Lesson &amp; Outcomes</h2>
  <Row className="sect-row">
-    <Col md={3} className="sub-section"><h3 className="title-border">Throughts</h3></Col>
+    <Col md={3} className="sub-section"><h3 className="title-border">Thoughts</h3></Col>
     <Col md={8} className="sub-content">
       <p>As this is something new and an experiment we will see how many people use it. To start with we will have 10,000 users a 5% response rate is
          normal for surveys and user feedback on dCloud apps so we can measure it against that.</p>
