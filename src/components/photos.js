b1192f227b481fd15ade2a197af0bd863409ea00
Shabia <shabz19@gmail.com>
2021-04-23 15:17:26 +0100

    photo update

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,63 +1,63 @@
 export const photos = [
   {
-    src: "https://instagram.flhr2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/49848215_385097298911061_4341922624353058091_n.jpg?_nc_ht=instagram.flhr2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=U_eDAigLU6EAX8XOGZz&tp=1&oh=d2e0655a6d5838af06631d971277c23e&oe=605BFAFD",
+    src: "https://www.dropbox.com/s/5c3pzc89q6ahtce/ig6.jpg?raw=1",
     width: 3,
     height: 4
   },
   {
-    src: "https://instagram.flhr2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/134853327_156238895947693_2922301211055484300_n.jpg?_nc_ht=instagram.flhr2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=SQ2AcVtLtgsAX_AnGMx&tp=1&oh=8c37c028de36eb2d1e4739af67a23a31&oe=605AC6C3",
+    src: "https://www.dropbox.com/s/bkb4mrsha0vwydq/ig4.jpg?raw=1",
     width: 3,
     height: 4
   },
   {
-    src: "https://instagram.flhr2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/135120751_830946007742629_8826424721011065380_n.jpg?_nc_ht=instagram.flhr2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=QmRDlEgcBeIAX94pSuA&tp=1&oh=6e9d0c327d610e35d9c3af020367da00&oe=605C118D",
+    src: "https://www.dropbox.com/s/dfykm5mpogxl3uo/ig1.jpg?raw=1",
     width: 3,
     height: 4
   },
   {
-    src: "https://instagram.flhr2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/133272811_407581033659873_7395446377046961853_n.jpg?_nc_ht=instagram.flhr2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=3amXX72BWIgAX9z0Mc0&tp=1&oh=b1091f78f45194f5bb75e1b47a389183&oe=605C3E8C",
-    width: 3,
-    height: 4
-  },
-
-  {
-    src: "https://instagram.flhr2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/120032147_228314675376035_4067279080333129996_n.jpg?_nc_ht=instagram.flhr2-2.fna.fbcdn.net&_nc_cat=111&_nc_ohc=aJMR6DK-0XgAX8oXs8G&tp=1&oh=76d2e860b4898d9aecbd4f0fac9e5add&oe=605CE6BB",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://instagram.flhr2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/106085874_695798564329576_7254655796373044981_n.jpg?_nc_ht=instagram.flhr2-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=IGO_mERUU6MAX-4oGy1&tp=1&oh=e610cf84e12d258383e7395d560ce1e3&oe=605989A2",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://instagram.flhr2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/117197879_306900507220950_8639308061204908701_n.jpg?_nc_ht=instagram.flhr2-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=3949MNxj0GwAX9q1sdi&tp=1&oh=ec423762d309c21a594ce7e682ddb602&oe=605CC4A0",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://instagram.flhr2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/70021071_405359470419404_5110259518493459437_n.jpg?_nc_ht=instagram.flhr2-2.fna.fbcdn.net&_nc_cat=108&_nc_ohc=XIDu208e6F0AX_qQVGE&tp=1&oh=842e6b444cff28b4e73428aaaebd98d1&oe=605AEBA9",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://instagram.flhr2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/47286148_756453791420054_2093714334539127864_n.jpg?_nc_ht=instagram.flhr2-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=jfoXoNf1zfgAX8svBzv&tp=1&oh=062f47ded43bf2addcc74dc1d863aad3&oe=605BBB17",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://instagram.flhr2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/75324255_188654078974564_8637533596987928785_n.jpg?_nc_ht=instagram.flhr2-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=5o9xulg4BScAX_ZUX83&tp=1&oh=2f2373e62d0daa9cd6b89edccefd5562&oe=605AB9BC",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://instagram.flhr2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/77018145_1233611260170502_5038589468359318434_n.jpg?_nc_ht=instagram.flhr2-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=3lHTPiS6ftIAX_sgO6d&tp=1&oh=fbc8dd7d0a1d2367c02ea94d5161a5c4&oe=605A9BE8",
+    src: "https://www.dropbox.com/s/4ttnk9c5886ew2b/ig8.jpg?raw=1",
     width: 3,
     height: 4
   },
 
   {
-    src: "https://instagram.flhr2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/83456053_2709948595766706_838466107438171476_n.jpg?_nc_ht=instagram.flhr2-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=rxe70rQLmA0AX_2FD1-&tp=1&oh=9c8a10f1e7e311a648880875599f289e&oe=6059B446",
+    src: "https://www.dropbox.com/s/k5jwnthdcxgk8kw/ig13.jpg?raw=1",
+     width: 3,
+    height: 4
+  },
+  {
+    src: "https://www.dropbox.com/s/ryifmij8mew6r7h/ig7.jpg?raw=1",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://www.dropbox.com/s/gkj5gwhvdkev91t/ig3.jpg?raw=1",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://www.dropbox.com/s/2fqiqw20rfzokro/ig15.jpg?raw=1",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://www.dropbox.com/s/slb5ulzr6npwyn6/ig9.jpg?raw=1",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://www.dropbox.com/s/98wruj05afqldd7/ig11.jpg?raw=1",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://www.dropbox.com/s/9uttuw96t96wiqq/ig2.jpg?raw=1",
+    width: 3,
+    height: 4
+  },
+
+  {
+    src: "https://www.dropbox.com/s/c6mccisti98lv40/ig12.jpg?raw=1",
     width: 3,
     height: 4
   }
